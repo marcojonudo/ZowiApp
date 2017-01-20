fa50f7c1b7d222c8317f7537f573c88cb301396b
Marco <marcomartinezavila@gmail.com>
2017-01-20 20:03:13 +0100

    Implementation of the dynamic creation of the layout using parameters. The array in activities_detailes is used to get all the data the activity needs to be created: type (grid, teeth...), description (for the text below the title), etc.

--- app/src/main/java/zowiapp/zowi/marco/zowiapp/GameParameters.java
+++ app/src/main/java/zowiapp/zowi/marco/zowiapp/GameParameters.java
@@ -1,33 +1,94 @@
 package zowiapp.zowi.marco.zowiapp;
 
 import android.content.Context;
-import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.FrameLayout;
 import android.widget.GridLayout;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
 import android.widget.Toast;
 
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+import org.w3c.dom.Text;
+
 public class GameParameters extends AppCompatActivity {
+
+    private LayoutInflater inflater;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_game_parameters);
+        setContentView(R.layout.basic_activity_template);
 
-        Bundle b = getIntent().getExtras();
-        Toast.makeText(this, String.valueOf(b.getInt("grid")), Toast.LENGTH_SHORT).show();
+        inflater = (LayoutInflater) getSystemService(Context.LAYOUT_INFLATER_SERVICE);
 
-        ViewGroup viewGroup = (ViewGroup) findViewById(R.id.game_parameters_container);
-        LayoutInflater inflater = (LayoutInflater) getSystemService(Context.LAYOUT_INFLATER_SERVICE);
-        View view = inflater.inflate(R.layout.layout_elements, viewGroup, false);
+        Bundle parameter = getIntent().getExtras();
+        String activityTitle = parameter.getString("activityTitle");
+        int activityNumber = parameter.getInt("activityNumber");
 
-        GridLayout grid = (GridLayout) view.findViewById(R.id.game_grid_3x3);
+        Toast.makeText(this, String.valueOf(activityNumber), Toast.LENGTH_SHORT).show();
 
-        Log.i("d","dd");
+        String[] activitiesDetails = getResources().getStringArray(R.array.activities_details);
+
+        try {
+            String json = activitiesDetails[activityNumber];
+            JSONObject activityDetails = new JSONObject(json);
+            String activityDescription = activityDetails.getString("description");
+
+            setTitleDescription(activityTitle, activityDescription);
+
+            switch (activityDetails.getInt("type")) {
+                case 1:
+                    int gridSize = activityDetails.getInt("gridSize");
+                    JSONArray jsonCoordinates = activityDetails.getJSONArray("coordinates");
+                    int[] coordinates = new int[jsonCoordinates.length()];
+                    for (int i=0; i<jsonCoordinates.length(); i++) {
+                        coordinates[i] = (int) jsonCoordinates.get(i);
+                    }
+
+                    generateGridActivity(gridSize, coordinates);
+                    break;
+                default:
+                    break;
+            }
+        }
+        catch (JSONException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private void setTitleDescription(String activityTitle, String activityDescription) {
+        TextView title = (TextView) findViewById(R.id.activity_title);
+        TextView description = (TextView) findViewById(R.id.activity_description);
+
+        title.setText(activityTitle);
+        description.setText(activityDescription);
+    }
+
+    private void generateGridActivity(int gridSize, int[] coordinates) {
+        RelativeLayout contentContainer = (RelativeLayout) findViewById(R.id.content_container);
+        RelativeLayout gridActivityTemplate = (RelativeLayout) inflater.inflate(R.layout.grid_activity_template, contentContainer, false);
+
+        switch (gridSize) {
+            /* 3x3 */
+            case 1:
+                /* The grid is added automatically to gridActivityTemplate because the third parameter is 'true' */
+                inflater.inflate(R.layout.grid_3x3_template, gridActivityTemplate, true);
+                break;
+            /* 4x4 */
+            case 2:
+                inflater.inflate(R.layout.grid_4x4_template, gridActivityTemplate, true);
+                break;
+            default:
+                break;
+        }
+
+        contentContainer.addView(gridActivityTemplate);
     }
 }
