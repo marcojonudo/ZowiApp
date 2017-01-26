9c134a399bf47e5915a979ba9ce192cb694d9e9c
Marco <marcomartinezavila@gmail.com>
2017-01-26 11:06:00 +0100

    Added support for the second type of operations activity. In this case, all the information is sent to Zowi, who shows it to the kid, and images are used in the operations

--- app/src/main/java/zowiapp/zowi/marco/zowiapp/activities/ActivityConstants.java
+++ app/src/main/java/zowiapp/zowi/marco/zowiapp/activities/ActivityConstants.java
@@ -44,5 +44,7 @@ public class ActivityConstants {
         protected static final String JSON_PARAMETER_OPERATIONSTYPE = "operationsType";
         protected static final String JSON_PARAMETER_IMAGE = "image";
         protected static final String JSON_PARAMETER_OPERATIONS = "operations";
+        protected static final String JSON_PARAMETER_OPERATIONSIMAGES = "operationsImages";
+        protected static final int OPERATION_IMAGE_WIDTH_PX = 125;
     }
 }
--- app/src/main/java/zowiapp/zowi/marco/zowiapp/activities/OperationsActivity.java
+++ app/src/main/java/zowiapp/zowi/marco/zowiapp/activities/OperationsActivity.java
@@ -31,11 +31,11 @@ public class OperationsActivity extends ActivityTemplate {
     private GameParameters gameParameters;
     private LayoutInflater inflater;
     private String activityTitle, activityDescription;
-    private int operationsType, operat;
+    private int operationsType;
     private String image;
     private String[][] operations;
+    private String[] operationsImages;
     private JSONObject activityDetails;
-    private int[][] coordinates;
 
     public OperationsActivity(GameParameters gameParameters, String activityTitle, JSONObject activityDetails) {
         this.gameParameters = gameParameters;
@@ -54,11 +54,19 @@ public class OperationsActivity extends ActivityTemplate {
             image = activityDetails.getString(OperationsConstants.JSON_PARAMETER_IMAGE);
             JSONArray jsonOperations = activityDetails.getJSONArray(OperationsConstants.JSON_PARAMETER_OPERATIONS);
             operations = new String[jsonOperations.length()][3];
+            JSONArray jsonOperationsImages = activityDetails.getJSONArray(OperationsConstants.JSON_PARAMETER_OPERATIONSIMAGES);
+            operationsImages = new String[jsonOperationsImages.length()];
 
             for (int i=0; i<jsonOperations.length(); i++) {
                 JSONArray operation = jsonOperations.getJSONArray(i);
                 for (int j=0; j<3; j++) {
                     operations[i][j] = operation.getString(j);
+                }
+            }
+
+            if (jsonOperationsImages.length() != 0) {
+                for (int i=0; i<jsonOperationsImages.length(); i++) {
+                    operationsImages[i] = jsonOperationsImages.getString(i);
                 }
             }
 
@@ -77,12 +85,14 @@ public class OperationsActivity extends ActivityTemplate {
         LinearLayout operationsActivityTemplate = (LinearLayout) inflater.inflate(R.layout.operations_activity_template, contentContainer, false);
         LinearLayout operationsContainer = (LinearLayout) operationsActivityTemplate.findViewById(R.id.operations_container);
 
+        /* Set the resource of the left image */
         ImageView mainImage = (ImageView) operationsActivityTemplate.findViewById(R.id.mainImage);
         mainImage.setImageResource(gameParameters.getResources().getIdentifier(image, "drawable", gameParameters.getPackageName()));
 
         switch (operationsType) {
             /* Numbers and operations are predefined */
             case 1:
+                /* No need to take care about operations images */
                 for (String[] operation: operations) {
                     LinearLayout operations1Template = (LinearLayout) inflater.inflate(R.layout.operations_1_template, operationsActivityTemplate, false);
 
@@ -97,7 +107,24 @@ public class OperationsActivity extends ActivityTemplate {
             /* Zowi gives the numbers and operation */
             case 2:
                 /* In this case, the numbers and operation must be sent to Zowi,
-                   instead of been displayd inmediatly on screen */
+                   instead of been displayed immediately on screen */
+                for (int i=0; i<operations.length; i++) {
+                    String[] operation = operations[i];
+                    LinearLayout operations2Template = (LinearLayout) inflater.inflate(R.layout.operations_2_template, operationsActivityTemplate, false);
+
+                    for (int j=0; j<operation.length; j++) {
+                        // Send to Zowi
+
+                        /* This operation selects automatically elements 2, 5 and 8, that correspond to
+                           to the ImageViews*/
+                        ImageView operationsImage = (ImageView) operations2Template.getChildAt(j+(2*(j+1))-1);
+                        operationsImage.setImageResource(gameParameters.getResources().getIdentifier(operationsImages[i], "drawable", gameParameters.getPackageName()));
+                        LinearLayout.LayoutParams layoutParams = new LinearLayout.LayoutParams(OperationsConstants.OPERATION_IMAGE_WIDTH_PX, OperationsConstants.OPERATION_IMAGE_WIDTH_PX);
+                        operationsImage.setLayoutParams(layoutParams);
+                    }
+
+                    operationsContainer.addView(operations2Template);
+                }
                 break;
             default:
                 break;
