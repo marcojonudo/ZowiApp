89266638e30e71250e79b888cf115595fb49b9ce
Marco <marcomartinezavila@gmail.com>
2017-02-02 12:09:34 +0100

    Removed some unused constants

--- app/src/main/java/zowiapp/zowi/marco/zowiapp/activities/ActivityConstants.java
+++ app/src/main/java/zowiapp/zowi/marco/zowiapp/activities/ActivityConstants.java
@@ -89,9 +89,6 @@ public class ActivityConstants {
     protected static class MemoryConstants {
         protected static final ActivityType MEMORY_TYPE = ActivityType.MEMORY;
         protected static final String JSON_PARAMETER_IMAGES = "images";
-        protected static final String JSON_PARAMETER_CONTAINERELEMENTS = "containerElements";
-        protected static final String JSON_PARAMETER_TEXTS = "texts";
-        protected static final String JSON_PARAMETER_CONTAINERIMAGES = "containerImages";
-        protected static final int DISTANCE_LIMIT = 100;
+
     }
 }
