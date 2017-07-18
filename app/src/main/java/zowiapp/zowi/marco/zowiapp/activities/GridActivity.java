f240d35dd4cc5421b734596fc9d85ef48448b524
Marco <marcomartinezavila@gmail.com>
2017-07-18 21:39:20 +0200

    Used common constants in GridActivity

--- app/src/main/java/zowiapp/zowi/marco/zowiapp/activities/GridActivity.java
+++ app/src/main/java/zowiapp/zowi/marco/zowiapp/activities/GridActivity.java
@@ -1,9 +1,8 @@
 package zowiapp.zowi.marco.zowiapp.activities;
 
-import android.content.Context;
+import android.graphics.Point;
 import android.graphics.drawable.Drawable;
 import android.support.constraint.ConstraintLayout;
-import android.view.LayoutInflater;
 import android.view.MotionEvent;
 import android.view.View;
 import android.view.ViewGroup;
@@ -28,20 +27,12 @@ import zowiapp.zowi.marco.zowiapp.listeners.TouchListener;
  */
 public class GridActivity extends ActivityTemplate {
 
-    private GameParameters gameParameters;
-    private LayoutInflater inflater;
-    private String activityTitle, activityDescription;
-    private String[] images;
     private int[] cells;
-    private int gridSize, cellWidth, cellHeight;
-    private JSONObject activityDetails;
-    private int[][] coordinates;
+    private int gridSize;
+    private Point cellDimensions;
 
     public GridActivity(GameParameters gameParameters, String activityTitle, JSONObject activityDetails) {
-        this.gameParameters = gameParameters;
-        this.activityTitle = activityTitle;
-        this.activityDetails = activityDetails;
-        this.inflater = (LayoutInflater) gameParameters.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+        initialiseCommonConstants(gameParameters, activityTitle, activityDetails);
 
         getParameters();
     }
@@ -53,12 +44,13 @@ public class GridActivity extends ActivityTemplate {
             gridSize = activityDetails.getInt(GridConstants.JSON_PARAMETER_GRIDSIZE);
             JSONArray jsonCells = activityDetails.getJSONArray(GridConstants.JSON_PARAMETER_CELLS);
             JSONArray jsonImages = activityDetails.getJSONArray(GridConstants.JSON_PARAMETER_IMAGES);
+
             cells = new int[jsonCells.length()];
-            images = new String[jsonImages.length()];
+            arrayImages = new String[jsonImages.length()];
 
             for (int i=0; i<jsonCells.length(); i++) {
                 cells[i] = jsonCells.getInt(i);
-                images[i] = jsonImages.getString(i);
+                arrayImages[i] = jsonImages.getString(i);
             }
 
             generateLayout();
@@ -81,19 +73,19 @@ public class GridActivity extends ActivityTemplate {
             case 1:
                 /* The grid is added automatically to 'grid' because the third parameter is 'true' */
                 inflater.inflate(R.layout.grid_3x3_template, grid, true);
-                coordinates = new int[GridConstants.COORDINATES_3X3_LENGTH][CommonConstants.AXIS_NUMBER];
+                imagesCoordinates = createEmptyPointArray(GridConstants.COORDINATES_3X3_LENGTH);
                 break;
             /* 4x4 */
             case 2:
                 inflater.inflate(R.layout.grid_4x4_template, grid, true);
-                coordinates = new int[GridConstants.COORDINATES_4X4_LENGTH][CommonConstants.AXIS_NUMBER];
+                imagesCoordinates = createEmptyPointArray(GridConstants.COORDINATES_4X4_LENGTH);
                 break;
             default:
                 break;
         }
 
         /* Set the listener that detects which section of the controls has been touched */
-        /* Instead of setting the listener on 'controls', it is set on both images. This way, we don't
+        /* Instead of setting the listener on 'controls', it is set on both arrayImages. This way, we don't
            have problems with the width of the rescaled image */
         ImageView innerControl = (ImageView) gridActivityTemplate.findViewById(R.id.inner_control);
         ImageView outerControl = (ImageView) gridActivityTemplate.findViewById(R.id.outer_control);
@@ -123,17 +115,15 @@ public class GridActivity extends ActivityTemplate {
             int left = gridContainer.getLeft();
             int top = gridContainer.getTop();
 
-            /* Cell center coordinates are calculated based on the upper left corner of the grid */
+            /* Cell center imagesCoordinates are calculated based on the upper left corner of the grid */
             for (int i=0; i<gameGrid.getChildCount(); i++) {
                 View cell = gameGrid.getChildAt(i);
-                coordinates[i][0] = left + cell.getLeft() + cell.getWidth()/2;
-                coordinates[i][1] = top + cell.getTop() + cell.getHeight()/2;
+                imagesCoordinates[i].set(left + cell.getLeft() + cell.getWidth()/2, top + cell.getTop() + cell.getHeight()/2);
 
-                cellWidth = cell.getWidth();
-                cellHeight = cell.getHeight();
+                cellDimensions.set(cell.getWidth(), cell.getHeight());
             }
 
-            placeImages(contentContainer, cells, images);
+            placeImages(contentContainer, cells, arrayImages);
         }
         else {
             new NullElement(gameParameters, this.getClass().getSimpleName(), Thread.currentThread().getStackTrace()[2].getMethodName(), "gridContainer");
@@ -142,9 +132,9 @@ public class GridActivity extends ActivityTemplate {
 
     private void placeImages(RelativeLayout contentContainer, int[] cells, String[] images) {
         /* 'cells' contains a number between 1 and 9 or 16 that indicates the cells that will contain an image */
-        /* 'images' contains the name of the resources */
+        /* 'arrayImages' contains the name of the resources */
         for (int i=0; i<cells.length; i++) {
-            placeImage(contentContainer, images[i], coordinates[cells[i]-1][0], coordinates[cells[i]-1][1]);
+            placeImage(contentContainer, images[i], imagesCoordinates[cells[i]-1].x, imagesCoordinates[cells[i]-1].y);
         }
 
     }
@@ -156,10 +146,10 @@ public class GridActivity extends ActivityTemplate {
         Drawable drawable = image.getDrawable();
         float scaleFactor;
         if (drawable.getIntrinsicWidth() > drawable.getIntrinsicHeight()) {
-            scaleFactor = (cellWidth*GridConstants.CELL_FILLED_SPACE) / drawable.getIntrinsicWidth();
+            scaleFactor = (cellDimensions.x*GridConstants.CELL_FILLED_SPACE) / drawable.getIntrinsicWidth();
         }
         else {
-            scaleFactor = (cellHeight*GridConstants.CELL_FILLED_SPACE) / drawable.getIntrinsicHeight();
+            scaleFactor = (cellDimensions.y*GridConstants.CELL_FILLED_SPACE) / drawable.getIntrinsicHeight();
         }
 
         int width = (int)(drawable.getIntrinsicWidth() * scaleFactor);
